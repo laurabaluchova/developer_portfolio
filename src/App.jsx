2aaf6931ed386f1fc7ec959692cc87058ca9825a
laura.baluchova <laura.menova@gmail.com>
2025-01-26 16:34:29 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Homepage from "./Components/Homepage";
+import ParticlesBg from 'particles-bg';
+
+function App() {
+  
+  return (
+    <>
+    <ParticlesBg color="#A7A7A7" type="cobweb" bg={true} />
+    <Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,14 @@
+function Homepage() {
+    return (
+       <div className="flex flex-col items-center justify-center h-screen px-4">
+  <p className="text-3xl sm:text-5xl">Hello World,</p>
+  <p className="text-4xl sm:text-6xl md:text-7xl font-bold text-center animate-fade-in">
+    I am Laura!
+  </p>
+  {/* <p className="text-l m-3 mt-6">After two and a half years of experience with UX design and research I decided to extend my knowledge and focus on frontend development with React. </p> */}
+</div>
+
+    )
+};
+
+export default Homepage;
