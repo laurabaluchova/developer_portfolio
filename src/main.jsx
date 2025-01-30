5ff9c542e84d0d60a07b4cab79349cdd164f3213
laura.baluchova <laura.menova@gmail.com>
2025-01-30 13:31:00 +0100

    fix scroll to top when redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,36 @@
-import { StrictMode } from 'react'
+import { StrictMode, useLayoutEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import About from './Components/About.jsx';
 import Homepage from './Components/Homepage.jsx';
 import Navbar from './Components/Navbar.jsx';
 import ProjectsList from './Components/Projects/ProjectsList.jsx';
 import projectsData  from './Data/projects.js';
 
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    // Scroll to the top of the page when the route changes
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location.pathname]);
+
+  return children;
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
+      <Wrapper>
     <App />      
     <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />    
         <Route path="/projects" element={<ProjectsList projects={projectsData}/>} />     
       </Routes>
-    
+      </Wrapper>
     </Router>
   </StrictMode>,
 )
